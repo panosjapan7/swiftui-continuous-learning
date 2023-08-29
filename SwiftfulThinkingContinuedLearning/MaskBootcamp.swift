022316346a99df105feb54103982760ce01d3c42
panosjapan7 <panosjapan@gmail.com>
2023-08-29 17:09:10 +0200

    8. How to use Mask in SwiftUI to create a 5-star rating button

---
+++ SwiftfulThinkingContinuedLearning/MaskBootcamp.swift
@@ -0,0 +1,49 @@
+//  MaskBootcamp.swift
+
+import SwiftUI
+
+struct MaskBootcamp: View {
+    
+    @State var rating: Int = 0
+    
+    var body: some View {
+        ZStack {
+            starsView
+                .overlay(overlayView.mask(starsView))
+        }
+    }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+//                    .foregroundColor(.yellow)
+                    .fill(LinearGradient(colors: [Color.red, Color.blue], startPoint: .leading, endPoint: .trailing))
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        // users can't tap on the overlay layer now
+        .allowsHitTesting(false)
+    }
+    
+    private var starsView: some View {
+        HStack {
+            ForEach(1..<6){ index in
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.gray)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            rating = index
+                        }
+                    }
+            }
+        }
+    }
+}
+
+struct MaskBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        MaskBootcamp()
+    }
+}
