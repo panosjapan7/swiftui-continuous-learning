12de91f20723b979ee7f5c05bed116c139cab401
panosjapan7 <panosjapan@gmail.com>
2023-08-30 08:50:59 +0200

    12. What is Hashable protocol in SwiftUI?

---
+++ SwiftfulThinkingContinuedLearning/HashableBootcamp.swift
@@ -0,0 +1,40 @@
+//  HashableBootcamp.swift
+
+import SwiftUI
+
+struct MyCustomModel: Hashable {
+    let title: String
+    let subtitle: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(title + subtitle) // this will create a unique hashable value based on the tittle + subtitle
+    }
+}
+
+struct HashableBootcamp: View {
+    
+    let data: [MyCustomModel] = [
+        MyCustomModel(title: "ONE"),
+        MyCustomModel(title: "TWO"),
+        MyCustomModel(title: "THREE"),
+        MyCustomModel(title: "FOUR"),
+        MyCustomModel(title: "FIVE"),
+    ]
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 40) {
+                ForEach(data, id: \.self) { item in
+                    Text(item.hashValue.description)
+                        .font(.headline)
+                }
+            }
+        }
+    }
+}
+
+struct HashableBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        HashableBootcamp()
+    }
+}
