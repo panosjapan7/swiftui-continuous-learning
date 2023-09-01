f65432d4593f1137d051268d2e2aa8cdd8d70ddf
panosjapan7 <panosjapan@gmail.com>
2023-09-01 10:26:01 +0200

    25. Publishers and Subscribers in Combine with a SwiftUI project

---
+++ SwiftfulThinkingContinuedLearning/SubscriberBootcamp.swift
@@ -0,0 +1,130 @@
+//  SubscriberBootcamp.swift
+
+import SwiftUI
+import Combine
+
+class SubscriberViewModel: ObservableObject {
+    @Published var count: Int = 0
+    
+    @Published var textFieldText: String = ""
+    @Published var textIsValid: Bool = false
+    @Published var showButton: Bool = false
+    
+    // Set is an array that can't have duplicates
+    //    var timer: AnyCancellable?
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        setUpTimer()
+        addTextFieldSubscriber()
+        addButtonSubscriber()
+    }
+    
+    func addTextFieldSubscriber() {
+        $textFieldText
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map{ (text) -> Bool in
+                if text.count > 3 {
+                    return true
+                }
+                return false
+            }
+            .sink(receiveValue: { [weak self] isValid in
+                self?.textIsValid = isValid
+            })
+            .store(in: &cancellables)
+    }
+    
+    func setUpTimer() {
+        Timer
+            .publish(every: 1, on: .main, in: .common)
+            .autoconnect() // starts the timer
+            .sink { [weak self] _ in
+                //check first if count is valid
+                guard let self = self else { return }
+                
+                self.count += 1
+                
+//                if self.count >= 10{
+//                    for item in self.cancellables {
+//                        item.cancel()
+//                    }
+//                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    func addButtonSubscriber() {
+        // We listen to 2 different publishers (textIsValid, count)
+        // and getting the most recent values from those publishers.
+        $textIsValid
+            .combineLatest($count)
+            .sink { [weak self] (isValid, count) in
+                guard let self = self else { return }
+                if isValid && count >= 10 {
+                    self.showButton = true
+                }
+                else {
+                    self.showButton = false
+                }
+            }
+            .store(in: &cancellables)
+    }
+}
+
+struct SubscriberBootcamp: View {
+    
+    @StateObject var vm = SubscriberViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("\(vm.count)")
+                .font(.largeTitle)
+            
+//            Text(vm.textIsValid.description)
+            
+            TextField("type something here...", text: $vm.textFieldText)
+                .padding(.leading)
+                .frame(height: 55)
+                .font(.headline)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(10)
+                .overlay(
+                    ZStack {
+                        Image(systemName: "xmark")
+                            .foregroundColor(.red)
+                            .opacity(
+                                vm.textFieldText.count < 1 ? 0 :
+                                vm.textIsValid ? 0 : 1)
+                        
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.green)
+                            .opacity(vm.textIsValid ? 1 : 0)
+                    }
+                        .font(.title)
+                        .padding(.trailing)
+                    , alignment: .trailing
+                )
+            
+            Button(action: {}, label: {
+                Text("Submit".uppercased())
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .opacity(vm.showButton ? 1.0 : 0.5)
+            })
+            .disabled(!vm.showButton)
+            
+        }
+        .padding()
+    }
+}
+
+struct SubscriberBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SubscriberBootcamp()
+    }
+}
